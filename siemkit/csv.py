f782995e2f073ab1f625b415c8c07c88b2e86404
David Krasnitsky <dave@cybersiem.com>
2020-06-19 20:03:40 +0300

    Sketch for license inclusion and NOTICE file.

--- siemkit/csv.py
+++ siemkit/csv.py
@@ -1,3 +1,5 @@
+# License: Apache 2.0
+#
 #   Copyright © 2020 CyberSIEM ®
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +13,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
 
 import csv
 
