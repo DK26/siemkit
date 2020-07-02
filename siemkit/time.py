63d4072a6271ae08ef70f8fb8128a98bb26d1acc
David Krasnitsky <dave@cybersiem.com>
2020-07-03 01:21:16 +0300

    Added `Timestamp` interface and `TimeTypes` enums for compatibility and extendability with \ and to different timestamp types.

--- siemkit/time.py
+++ siemkit/time.py
@@ -19,39 +19,71 @@ from datetime import tzinfo
 from math import floor
 from enum import Enum
 from abc import ABC
+from abc import abstractmethod
+from inspect import isclass
 import time
 
 UTC = timezone.utc
 
-DEFAULT = {
-    'format': "%b %d %Y %H:%M:%S",
-    'tz': None
-}
-
 
 class Timestamp(ABC):
+    """
+    An interface for implementing a proper Timestamp.
+        Once you implement it, make sure you also define it in the `TimeType` enum class.
+    """
 
     @classmethod
-    def from_datetime(cls, datetime_, tz=None):
+    @abstractmethod
+    def from_datetime(cls, datetime_: datetime, tz: tzinfo = None) -> int:
+        """
+        A static method to convert a datetime into a timestamp of this time type.
+        :param datetime_:
+        :param tz: Timezone to convert to. Default is None.
+        :return:
+        """
         pass
 
     @classmethod
-    def to_datetime(cls, timestamp, tz=None):
+    @abstractmethod
+    def to_datetime(cls, timestamp: int, tz: tzinfo = None) -> datetime:
+        """
+        A static method to convert a timestamp of this time type into a datetime.
+        :param timestamp:
+        :param tz: Timezone of the the timestamp to let the datetime know its timezone.
+        :return:
+        """
         pass
 
     @classmethod
-    def delta(cls, *args, **kwargs):
+    @abstractmethod
+    def delta(cls, *args, **kwargs) -> int:
+        """
+        A static method to convert time delta arguments into time units of this time type.
+
+        e.g.
+            delta(days=3)
+            delta(days=365 * 3)  # 3 Years in milliseconds (default TimeType: time.TimeType.EPOCH_MILLIS).
+
+            delta(days=3, type_=time.TimeType.EPOCH)  # 3 days in seconds (time.TimeType.EPOCH).
+
+            All arguments are passed to the underlying timedelta() object.
+                delta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
         pass
 
 
 class EpochTimestamp(Timestamp):
 
     @classmethod
-    def from_datetime(cls, datetime_, tz=None) -> int:
+    def from_datetime(cls, datetime_: datetime, tz: tzinfo = None) -> int:
         return floor(datetime_.astimezone(tz=tz).timestamp())
 
     @classmethod
-    def to_datetime(cls, timestamp, tz=None) -> datetime:
+    def to_datetime(cls, timestamp: int, tz: tzinfo = None) -> datetime:
         return datetime.fromtimestamp(timestamp).replace(tzinfo=tz)
 
     @classmethod
@@ -64,11 +96,11 @@ class EpochMillisTimestamp(Timestamp):
     PRECISION = 1_000
 
     @classmethod
-    def from_datetime(cls, datetime_, tz=None) -> int:
+    def from_datetime(cls, datetime_: datetime, tz: tzinfo = None) -> int:
         return floor(datetime_.astimezone(tz=tz).timestamp() * cls.PRECISION)
 
     @classmethod
-    def to_datetime(cls, timestamp, tz=None) -> datetime:
+    def to_datetime(cls, timestamp: int, tz: tzinfo = None) -> datetime:
         return datetime.fromtimestamp(int(timestamp) / cls.PRECISION).replace(tzinfo=tz)
 
     @classmethod
@@ -77,6 +109,10 @@ class EpochMillisTimestamp(Timestamp):
 
 
 class LDAPTimestamp(Timestamp):
+    """
+    Converts a Microsoft Win32 FILETIME timestamp (aka LDAP / Active Directory timestamp)
+     into & from a datetime object.
+    """
 
     LDAP_START_TIME = datetime(1601, 1, 1)
 
@@ -85,8 +121,7 @@ class LDAPTimestamp(Timestamp):
     PRECISION = 10_000_000
 
     @classmethod
-    def from_datetime(cls, datetime_, tz=None):
-        #datetime_ = utc_to_tz(datetime_, tz=None)  # Replace default UTC timezone to local (None).
+    def from_datetime(cls, datetime_: datetime, tz: tzinfo = None):
 
         datetime_ = datetime_.astimezone(tz=tz)
 
@@ -95,53 +130,19 @@ class LDAPTimestamp(Timestamp):
         return floor(ldap_timestamp)
 
     @classmethod
-    def to_datetime(cls, timestamp, tz=None):
+    def to_datetime(cls, timestamp: int, tz: tzinfo = None):
 
         epoch_datetime = cls.LDAP_START_TIME + timedelta(
             seconds=(timestamp / cls.PRECISION)
         )
 
+        epoch_datetime = epoch_datetime.replace(tzinfo=UTC).astimezone(tz=None)
+
         return epoch_datetime.replace(tzinfo=tz)
 
     @classmethod
     def delta(cls, *args, **kwargs):
-        pass
-
-
-def from_filetime(timestamp: int) -> datetime:
-    """
-    Converts a Microsoft Win32 FILETIME timestamp (aka LDAP / Active Directory timestamp)
-     into a datetime object.
-
-    :param timestamp: Microsoft Win32 FILETIME timestamp
-    :return:
-    """
-    filetime_start_time = datetime(1601, 1, 1)
-    extra_nanoseconds_precision = 10_000_000
-
-    epoch_datetime = filetime_start_time + timedelta(
-        seconds=(timestamp / extra_nanoseconds_precision)
-    )
-
-    return epoch_datetime
-
-
-def to_filetime(datetime_: datetime) -> int:
-    """
-    Converts a datetime object into
-     a Microsoft Win32 FILETIME timestamp (aka LDAP / Active Directory timestamp)
-
-    :param datetime_:
-    :return: Microsoft Win32 FILETIME timestamp
-    """
-    datetime_ = utc_to_tz(datetime_, tz=None)  # Replace default UTC timezone to local (None).
-
-    epoch_start_time = 116_444_736_000_000_000
-    extra_nanoseconds_precision = 10_000_000
-
-    filetime = (datetime_.timestamp() * extra_nanoseconds_precision) + epoch_start_time
-
-    return int(filetime)
+        return floor(timedelta(*args, **kwargs).total_seconds() * cls.PRECISION)
 
 
 class TimeType(Enum):
@@ -150,8 +151,28 @@ class TimeType(Enum):
     EPOCH_MILLIS = EpochMillisTimestamp
     LDAP = LDAPTimestamp
 
-    def __repr__(self):
-        return self.value
+
+default = {
+    'format': "%b %d %Y %H:%M:%S",
+    'tz': None,
+    'type': TimeType.EPOCH_MILLIS
+}
+
+
+def time_type(time_type_enum: TimeType) -> Timestamp:
+    """
+    Return reference to a Timestamp class of a given TimeType enum argument or the default Timestamp.
+    :param time_type_enum:
+    :return: Timestamp Class reference
+    """
+
+    if time_type_enum is None or not (isclass(time_type_enum.value)
+                                      and issubclass(time_type_enum.value, Timestamp)):
+        time_type_class = default['type'].value
+    else:
+        time_type_class = time_type_enum.value
+
+    return time_type_class
 
 
 def sleep(*args, **kwargs):
@@ -169,110 +190,47 @@ def sleep(*args, **kwargs):
     time.sleep(timedelta(*args, **kwargs).total_seconds())
 
 
-def to_millis(datetime_: datetime, tz=None) -> int:
-    """
-    Convert a datetime object to milliseconds.
-
-    :param datetime_:
-    :param tz:
-    :return: milliseconds epoch timestamp
-    """
-    return floor(utc_to_tz(datetime_, tz).timestamp() * 1000)
-
-
-def millis(*args, **kwargs) -> int:
-    """
-    Converts time delta parameters to milliseconds.
-
-    e.g.
-        millis(days=3)
-        millis(days=365 * 3)  # 3 Years in milliseconds
-
-        All arguments are passed to the underlying timedelta() object.
-            millis(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
-
-    :return milliseconds epoch timestamp
-    """
-    return floor(timedelta(*args, **kwargs).total_seconds() * 1000)
-
-
-def ago(*args, **kwargs) -> datetime:
+def ago(type_: TimeType = None, *args, **kwargs) -> int:
     """
     Calculates the date & time from now, in time delta parameters.
 
     e.g.
         ago(days=3)
-        ago(days=365 * 3)  # 3 Years ago
+        ago(days=365 * 3)  # 3 Years ago in milliseconds (default TimeType: time.TimeType.EPOCH_MILLIS).
+
+        ago(days=3, type_=time.TimeType.EPOCH)  # 3 days ago in seconds (time.TimeType.EPOCH).
 
     All arguments are passed to the underlying timedelta() object.
         ago(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
 
     :return A 'datetime' object.
     """
-    return datetime.now() - timedelta(*args, **kwargs)
+    return time_type(type_).from_datetime(datetime.now() - timedelta(*args, **kwargs))
 
 
-def dev_delta(type_=TimeType.EPOCH_MILLIS, *args, **kwargs) -> int:
-    return type_.value.delta(*args, **kwargs)
+def delta(type_: TimeType = None, *args, **kwargs) -> int:
+    return time_type(type_).delta(*args, **kwargs)
 
 
-def dev_to_timestamp(datetime_: datetime, tz=None, type_=TimeType.EPOCH_MILLIS) -> int:
-    return type_.value.from_datetime(datetime_, tz)
+def to_timestamp(datetime_: datetime, tz=None, type_: TimeType = None) -> int:
+    return time_type(type_).from_datetime(datetime_, tz)
 
 
-def dev_from_timestamp(timestamp: int, tz=None, type_=TimeType.EPOCH_MILLIS) -> datetime:
-    return type_.value.to_datetime(timestamp, tz)
-
-
-def to_timestamp(datetime_: datetime, utc: bool = False) -> int:
-    """
-    Turns a datetime object into a milliseconds epoch timestamp ("Java Timestamp").
-
-    :param datetime_: datetime object
-    :param utc: If True, converts the local time zone datetime object to a UTC timestamp.
-    :return: milliseconds epoch timestamp
-    """
-    ts = datetime_.timestamp()
-
-    if utc:
-        ts = (datetime.utcfromtimestamp(ts).timestamp())
-
-    return floor(ts * 1000)
-
-
-def from_timestamp(timestamp: 'int or str of milliseconds', localize: bool = False, tz: tzinfo = None) -> datetime:
-    """
-    Create a datetime object out of a milliseconds epoch timestamp ("Java Timestamp").
-
-     A default time zone can be assigned globally to `siemkit.time.DEFAULT['tz']`.
-
-    :param timestamp: Milliseconds epoch timestamp.
-    :param localize: If True, converts UTC timestamp to a localized datetime.
-    :param tz: Timezone for the UTC timestamp localization.
-                By default uses the local system time zone (None).
-    :return: A 'datetime' object.
-    """
-    ts = int(timestamp) / 1000
-
-    if localize:
-        if not tz:
-            tz = DEFAULT.get("tz")
-        return utc_to_tz(datetime.fromtimestamp(ts), tz=tz)
-
-    return datetime.fromtimestamp(ts)
+def from_timestamp(timestamp: int, tz=None, type_: TimeType = None) -> datetime:
+    return time_type(type_).to_datetime(timestamp, tz)
 
 
 def to_format(datetime_: datetime, format_: str = None) -> str:
     """
     Convert a datetime object to a default or given time format.
-     A default time format can be assigned globally to `siemkit.time.DEFAULT['format']`.
+     A default time format can be assigned globally to `siemkit.time.default['format']`.
 
     :param datetime_:
     :param format_:
     :return:
     """
     if not format_:
-        format_ = DEFAULT.get("format", "%b %d %Y %H:%M:%S")
+        format_ = default.get("format", "%b %d %Y %H:%M:%S")
 
     return datetime_.strftime(format_)
 
@@ -280,14 +238,14 @@ def to_format(datetime_: datetime, format_: str = None) -> str:
 def from_format(date_string: str, format_: str = None) -> datetime:
     """
     Format a time string with a default or given time format into a datetime object.
-     A default time format can be assigned globally to `siemkit.time.DEFAULT['format']`.
+     A default time format can be assigned globally to `siemkit.time.default['format']`.
 
     :param date_string:
     :param format_:
     :return:
     """
     if not format_:
-        format_ = DEFAULT.get("format", "%b %d %Y %H:%M:%S")
+        format_ = default.get("format", "%b %d %Y %H:%M:%S")
 
     return datetime.strptime(date_string, format_)
 
@@ -297,13 +255,13 @@ def utc_to_tz(datetime_: datetime, tz: tzinfo = None) -> datetime:
     Converts a UTC datetime object to a different time zone.
         by default will convert to the system's local time zone.
 
-     A default time zone can be assigned globally to `siemkit.time.DEFAULT['tz']`.
+     A default time zone can be assigned globally to `siemkit.time.default['tz']`.
 
     :param datetime_:
     :param tz:
     :return:
     """
     if not tz:
-        tz = DEFAULT.get("tz")
+        tz = default.get("tz")
 
     return datetime_.replace(tzinfo=timezone.utc).astimezone(tz=tz)
