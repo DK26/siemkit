65c086b465db4895c5dc1c564e9e200bfa982c9f
David Krasnitsky <dave@cybersiem.com>
2020-07-12 01:32:48 +0300

    Renamed `session.py` to `net.py` as some of the functions are 'connection-less' and do not maintain a session. Implemented all base connection types for sending data: Unicast: TCP/UDP, Multicast & Broadcast, both under the `send.py` library and the `net.py` library.

--- siemkit/send.py
+++ siemkit/send.py
@@ -15,42 +15,66 @@
 import socket
 import struct
 from telnetlib import Telnet
+from typing import Union
+from ipaddress import ip_address
+
+default = {
+    'unicode': 'utf-8'
+}
 
 
-def tcp(host: str, port: int, payload: bytes) -> int:
+def tcp(host: str, port: int, payload: Union[bytes, str], timeout: int = 3) -> int:
 
     if isinstance(payload, str):
-        payload = bytes(payload, 'utf-8')
+        payload = bytes(payload, default['unicode'])
 
-    with Telnet(host, port) as session:
+    with Telnet(host, port, timeout=timeout) as session:
         session.write(payload)
 
     return len(payload)
 
 
-def udp(host: str, port: int, payload: bytes) -> int:
+def udp(host: str, port: int, payload: Union[bytes, str], ttl=32) -> int:
 
     if isinstance(payload, str):
-        payload = bytes(payload, 'utf-8')
+        payload = bytes(payload, default['unicode'])
 
     address = host, port
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
     udp_socket.sendto(payload, address)
 
     return len(payload)
 
 
-def multicast(group: str, port: int, payload: bytes) -> int:
+def multicast(group: str, port: int, payload: Union[bytes, str], ttl=2) -> int:
+
+    # REF: https://stackoverflow.com/questions/603852/how-do-you-udp-multicast-in-python
 
     if isinstance(payload, str):
-        payload = bytes(payload, 'utf-8')
+        payload = bytes(payload, default['unicode'])
+
+    if not ip_address(group).is_multicast:
+        raise ValueError(f"Address '{group}' is not a multi-cast group.")
+
+    address = group, port
+    multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    multicast_socket.sendto(payload, address)
 
     return len(payload)
 
 
-def broadcast(port: int, payload: bytes) -> int:
+def broadcast(port: int, payload: Union[bytes, str], ttl: int = 1) -> int:
+
+    # REF: https://gist.github.com/ninedraft/7c47282f8b53ac015c1e326fffb664b5
 
     if isinstance(payload, str):
-        payload = bytes(payload, 'utf-8')
+        payload = bytes(payload, default['unicode'])
+
+    address = '255.255.255.255', port
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, ttl)
+    broadcast_socket.sendto(payload, address)
 
     return len(payload)
