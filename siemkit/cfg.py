42e7b8b63f9184c167f7ef5dbbccfe521f804d6e
David Krasnitsky <dikaveman@gmail.com>
2020-07-15 18:44:26 +0300

    Updated README.md

--- siemkit/cfg.py
+++ siemkit/cfg.py
@@ -1,4 +1,4 @@
-#   Copyright (C) 2020 CyberSIEM (R)
+#   Copyright (C) 2020 CyberSIEM(R)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
