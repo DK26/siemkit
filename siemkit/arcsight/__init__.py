eedc9e38b76c87e2c430c24046f34d4a4298acad
David Krasnitsky <dikaveman@gmail.com>
2020-07-29 12:56:11 +0300

    More ArcSight ESM API implementations. Simplified CEF from ArcSight.

--- siemkit/arcsight/__init__.py
+++ siemkit/arcsight/__init__.py
@@ -106,7 +106,7 @@ class Esm:
         )
 
         if response.status_code() == 200:
-            pass
+            return simplified_cef_events(response)
 
     def get_activelist_entries(self, resource_id):
 
@@ -118,6 +118,8 @@ class Esm:
         response = self.uri(
             ActiveListRequestEnum.GET_ENTRIES, variables
         )
+
+        return tuple(normalized_active_list_entries(response))
 
 
 def normalized_active_list_entries(response: HttpResponse):
@@ -135,264 +137,52 @@ def normalized_active_list_entries(response: HttpResponse):
         yield dict_entry
 
 
-"""
-{
-    "sev.getSecurityEventsResponse": {
-        "sev.return": {
-            "agent": {
-                "mutable": true,
-                "address": 174351991,
-                "addressAsBytes": "CmRmdw==",
-                "assetId": "4paH4P3EBABCSMnSiDweTZg==",
-                "assetLocalId": 17179869193,
-                "assetName": "linuxconn",
-                "hostName": "linuxconn",
-                "macAddress": 52229671306,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": "7.15.0.8295.0",
-                "id": "3CgN77nEBABDaCRMDAaJaBw==",
-                "name": "syslog_10525",
-                "type": "syslog"
-            },
-            "agentReceiptTime": 1595968655010,
-            "agentSeverity": 0,
-            "aggregatedEventCount": 1,
-            "assetCriticality": 0,
-            "baseEventCount": 1,
-            "bytesIn": -2147483648,
-            "bytesOut": -2147483648,
-            "concentratorAgents": {
-                "mutable": true,
-                "address": 174351991,
-                "addressAsBytes": "CmRmdw==",
-                "assetId": "4paH4P3EBABCSMnSiDweTZg==",
-                "assetLocalId": 17179869193,
-                "assetName": "linuxconn",
-                "hostName": "linuxconn",
-                "macAddress": 52229671306,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": "7.15.0.8295.0",
-                "id": "3CgN77nEBABDaCRMDAaJaBw==",
-                "name": "syslog_10525",
-                "type": "syslog"
-            },
-            "concentratorDevices": {
-                "mutable": true,
-                "address": 174351988,
-                "addressAsBytes": "CmRmdA==",
-                "assetId": "4+ZGOMnEBABCDkWcZFgAOeA==",
-                "assetLocalId": 17179869190,
-                "assetName": "10.100.102.116",
-                "macAddress": -9223372036854775808,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": 0,
-                "product": "SIEM Kit",
-                "vendor": "CyberSIEM(R) Community"
-            },
-            "correlatedEventCount": 0,
-            "device": {
-                "mutable": true,
-                "address": 174351988,
-                "addressAsBytes": "CmRmdA==",
-                "assetId": "4+ZGOMnEBABCDkWcZFgAOeA==",
-                "assetLocalId": 17179869190,
-                "assetName": "10.100.102.116",
-                "macAddress": -9223372036854775808,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": 0,
-                "product": "SIEM Kit",
-                "vendor": "CyberSIEM(R) Community"
-            },
-            "deviceCustomDate1": -9223372036854775808,
-            "deviceCustomDate2": -9223372036854775808,
-            "deviceCustomFloatingPoint1": 4.9E-324,
-            "deviceCustomFloatingPoint2": 4.9E-324,
-            "deviceCustomFloatingPoint3": 4.9E-324,
-            "deviceCustomFloatingPoint4": 4.9E-324,
-            "deviceCustomNumber1": -9223372036854775808,
-            "deviceCustomNumber2": -9223372036854775808,
-            "deviceCustomNumber3": -9223372036854775808,
-            "deviceDirection": -2147483648,
-            "deviceEventClassId": 100,
-            "deviceProcessId": -2147483648,
-            "deviceReceiptTime": 1595968655010,
-            "deviceSeverity": "Unknown",
-            "domainDate1": -9223372036854775808,
-            "domainDate2": -9223372036854775808,
-            "domainDate3": -9223372036854775808,
-            "domainDate4": -9223372036854775808,
-            "domainDate5": -9223372036854775808,
-            "domainDate6": -9223372036854775808,
-            "domainFp1": 4.9E-324,
-            "domainFp2": 4.9E-324,
-            "domainFp3": 4.9E-324,
-            "domainFp4": 4.9E-324,
-            "domainFp5": 4.9E-324,
-            "domainFp6": 4.9E-324,
-            "domainFp7": 4.9E-324,
-            "domainFp8": 4.9E-324,
-            "domainIpv4addr1": -9223372036854775808,
-            "domainIpv4addr2": -9223372036854775808,
-            "domainIpv4addr3": -9223372036854775808,
-            "domainIpv4addr4": -9223372036854775808,
-            "domainNumber1": -9223372036854775808,
-            "domainNumber10": -9223372036854775808,
-            "domainNumber11": -9223372036854775808,
-            "domainNumber12": -9223372036854775808,
-            "domainNumber13": -9223372036854775808,
-            "domainNumber2": -9223372036854775808,
-            "domainNumber3": -9223372036854775808,
-            "domainNumber4": -9223372036854775808,
-            "domainNumber5": -9223372036854775808,
-            "domainNumber6": -9223372036854775808,
-            "domainNumber7": -9223372036854775808,
-            "domainNumber8": -9223372036854775808,
-            "domainNumber9": -9223372036854775808,
-            "endTime": 1595968655010,
-            "eventAnnotation": {
-                "auditTrail": "1,1595435564387,root,Queued,,,,\n",
-                "flags": 0,
-                "modificationTime": 1595968683168,
-                "stage": {
-                    "id": "R9MHiNfoAABCASsxbPIxG0g==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2209,
-                    "referenceName": "Stage",
-                    "referenceString": "<Resource URI=\"/All Stages/Queued\" ID=\"R9MHiNfoAABCASsxbPIxG0g==\"/>",
-                    "referenceType": 34,
-                    "uri": "/All Stages/Queued"
-                },
-                "stageUpdateTime": 1595968683168,
-                "version": 1,
-                "endTime": 1595968655010,
-                "eventId": 120740877,
-                "managerReceiptTime": 1595968683168
-            },
-            "eventId": 120740877,
-            "finalDevice": {
-                "mutable": true,
-                "address": 174351988,
-                "addressAsBytes": "CmRmdA==",
-                "assetId": "4+ZGOMnEBABCDkWcZFgAOeA==",
-                "assetLocalId": 17179869190,
-                "assetName": "10.100.102.116",
-                "macAddress": -9223372036854775808,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": 0,
-                "product": "SIEM Kit",
-                "vendor": "CyberSIEM(R) Community"
-            },
-            "flexDate1": -9223372036854775808,
-            "flexNumber1": -9223372036854775808,
-            "flexNumber2": -9223372036854775808,
-            "locality": 0,
-            "managerId": -128,
-            "managerReceiptTime": 1595968683168,
-            "modelConfidence": 0,
-            "name": "MailService Development",
-            "originalAgent": {
-                "mutable": true,
-                "address": 174351991,
-                "addressAsBytes": "CmRmdw==",
-                "assetId": "4paH4P3EBABCSMnSiDweTZg==",
-                "assetLocalId": 17179869193,
-                "assetName": "linuxconn",
-                "hostName": "linuxconn",
-                "macAddress": 52229671306,
-                "translatedAddress": -9223372036854775808,
-                "zone": {
-                    "id": "ML8022AABABCDTFpYAT3UdQ==",
-                    "isModifiable": false,
-                    "managerID": "UUQVDXEBABCAXSH6KyOOWw==",
-                    "referenceID": 2084,
-                    "referenceName": "Zone",
-                    "referenceString": "<Resource URI=\"/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255\" ID=\"ML8022AABABCDTFpYAT3UdQ==\"/>",
-                    "referenceType": 29,
-                    "uri": "/All Zones/ArcSight System/Private Address Space Zones/RFC1918: 10.0.0.0-10.255.255.255"
-                },
-                "timeZone": "Asia/Jerusalem",
-                "version": "7.15.0.8295.0",
-                "id": "3CgN77nEBABDaCRMDAaJaBw==",
-                "name": "syslog_10525",
-                "type": "syslog"
-            },
-            "originator": "SOURCE",
-            "persistence": -2147483648,
-            "priority": 2,
-            "relevance": 10,
-            "sessionId": -9223372036854775808,
-            "severity": 0,
-            "source": {
-                "mutable": true,
-                "address": -9223372036854775808,
-                "assetLocalId": -9223372036854775808,
-                "macAddress": -9223372036854775808,
-                "translatedAddress": -9223372036854775808,
-                "port": -2147483648,
-                "processId": -2147483648,
-                "translatedPort": -2147483648,
-                "userName": "King Dave"
-            },
-            "startTime": 1595968655010,
-            "ttl": 10,
-            "type": "BASE"
-        }
-    }
-}
-"""
+def simple_key_value(complex_cef: dict, prev_key=None):
+
+    for key, value in complex_cef.items():
+
+        if isinstance(prev_key, str):
+            # This is a node
+            current_key = prev_key + key[0].upper() + key[1:]
+        else:
+            # This is the root
+            current_key = key
+
+        if isinstance(value, dict):
+            yield from simple_key_value(value, prev_key=current_key)
+        else:
+            yield current_key, value
+
+
+def simplify_cef(complex_cef: dict):
+
+    simple_cef = {}
+
+    for key, item in complex_cef.items():
+        if isinstance(item, dict):
+            for simple_key, simple_value in simple_key_value(item):
+                simple_cef[simple_key] = simple_value
+        else:
+            simple_cef[key] = item
+
+    return simple_cef
+
+
+def simplified_cef_events(response: HttpResponse):
+
+    if response.status_code() != 200:
+        return
+
+    response_json = response.json()
+    complex_cef_events = response_json['sev.getSecurityEventsResponse']['sev.return']
+
+    if isinstance(complex_cef_events, dict):
+
+        # Received a single event
+        yield simplify_cef(complex_cef_events)
+
+    else:
+
+        # Received multiple events
+        for event in complex_cef_events:
+            yield simplify_cef(event)
